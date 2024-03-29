e86d809a396d53eafde0b3d85fcbc3ae8831c0a2
Elie BIME <elie.bime@edu.ece.fr>
2024-03-29 16:57:09 +0100

    Correctifs OK

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,97 +30,116 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    bool isAllContentLoaded =
-        !isLoadingSeries && !isLoadingComics && !isLoadingMovies;
-
-    return Scaffold(
-      backgroundColor: const Color(0xFF152630),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF152630),
-        elevation: 0,
-        title: const Text(
-          'Bienvenue !',
-          style: TextStyle(
-            fontFamily: 'Nunito',
-            color: Colors.white,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<SeriesBloc, SeriesState>(
+          listener: (context, state) {
+            if (state is SeriesLoaded) {
+              setState(() => isLoadingSeries = false);
+            }
+          },
         ),
-        titleSpacing: 26,
-        centerTitle: false,
-      ),
-      body: Stack(
-        children: [
-          SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  BlocBuilder<SeriesBloc, SeriesState>(
-                    builder: (context, state) {
-                      if (state is SeriesLoaded) {
-                        isLoadingSeries = false;
-                        return ItemsListWidget(
-                          title: 'Séries populaires',
-                          items: state.series.take(5).toList(),
-                          onViewMorePressed: (index) {
-                            widget.onViewMorePressed(1);
-                          },
-                        );
-                      }
-                      return const SizedBox();
-                    },
-                  ),
-                  BlocBuilder<ComicsBloc, ComicsState>(
-                    builder: (context, state) {
-                      if (state is ComicsLoaded) {
-                        isLoadingComics = false;
-                        return ItemsListWidget(
-                          title: 'Comics populaires',
-                          items: state.comics.take(5).toList(),
-                          onViewMorePressed: (index) {
-                            widget.onViewMorePressed(2);
-                          },
-                        );
-                      }
-                      return const SizedBox();
-                    },
-                  ),
-                  BlocBuilder<MoviesBloc, MoviesState>(
-                    builder: (context, state) {
-                      if (state is MoviesLoaded) {
-                        isLoadingMovies = false;
-                        return ItemsListWidget(
-                          title: 'Films populaires',
-                          items: state.movies.take(5).toList(),
-                          onViewMorePressed: (index) {
-                            widget.onViewMorePressed(3);
-                          },
-                        );
-                      }
-                      return const SizedBox();
-                    },
-                  ),
-                ],
-              ),
+        BlocListener<ComicsBloc, ComicsState>(
+          listener: (context, state) {
+            if (state is ComicsLoaded) {
+              setState(() => isLoadingComics = false);
+            }
+          },
+        ),
+        BlocListener<MoviesBloc, MoviesState>(
+          listener: (context, state) {
+            if (state is MoviesLoaded) {
+              setState(() => isLoadingMovies = false);
+            }
+          },
+        ),
+      ],
+      child: Scaffold(
+        backgroundColor: const Color(0xFF152630),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF152630),
+          elevation: 0,
+          title: const Text(
+            'Bienvenue !',
+            style: TextStyle(
+              fontFamily: 'Nunito',
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          if (!isAllContentLoaded) ...[
-            const Center(
-              child: CircularProgressIndicator(),
+          titleSpacing: 26,
+          centerTitle: false,
+        ),
+        body: Stack(
+          children: [
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    BlocBuilder<SeriesBloc, SeriesState>(
+                      builder: (context, state) {
+                        if (state is SeriesLoaded) {
+                          isLoadingSeries = false;
+                          return ItemsListWidget(
+                            title: 'Séries populaires',
+                            items: state.series.take(5).toList(),
+                            onViewMorePressed: (index) {
+                              widget.onViewMorePressed(1);
+                            },
+                          );
+                        }
+                        return const SizedBox();
+                      },
+                    ),
+                    BlocBuilder<ComicsBloc, ComicsState>(
+                      builder: (context, state) {
+                        if (state is ComicsLoaded) {
+                          isLoadingComics = false;
+                          return ItemsListWidget(
+                            title: 'Comics populaires',
+                            items: state.comics.take(5).toList(),
+                            onViewMorePressed: (index) {
+                              widget.onViewMorePressed(2);
+                            },
+                          );
+                        }
+                        return const SizedBox();
+                      },
+                    ),
+                    BlocBuilder<MoviesBloc, MoviesState>(
+                      builder: (context, state) {
+                        if (state is MoviesLoaded) {
+                          isLoadingMovies = false;
+                          return ItemsListWidget(
+                            title: 'Films populaires',
+                            items: state.movies.take(5).toList(),
+                            onViewMorePressed: (index) {
+                              widget.onViewMorePressed(3);
+                            },
+                          );
+                        }
+                        return const SizedBox();
+                      },
+                    ),                  ],
+                ),
+              ),
+            ),
+            if (isLoadingSeries || isLoadingComics || isLoadingMovies)
+              const Center(child: CircularProgressIndicator()),
+            Positioned(
+              top: MediaQuery.of(context).padding.top,
+              right: 0,
+              child: Opacity(
+                opacity: 0.9,
+                child: SvgPicture.asset(
+                  'assets/images/astronaut.svg',
+                  // Set the width and height as needed
+                ),
+              ),
             ),
           ],
-          Positioned(
-            top: MediaQuery.of(context).padding.top,
-            right: 0,
-            child: Opacity(
-              opacity: 0.9,
-              child: SvgPicture.asset(
-                'assets/images/astronaut.svg',
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
